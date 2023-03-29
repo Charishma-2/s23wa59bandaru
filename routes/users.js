907e465cafe74672cc717842834239b150a42ff3
Charishma-2 <charishmachowdarybandaru@gmail.com>
2023-03-28 23:58:32 -0500

    edited user file

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var sum=0;
-var times=0;
+var  times=0;
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 
-  times=times+1;
-  sum=sum+times;
+  times+=1;
+  sum+=times;
   res.send('Sum is: '+sum);
 });
 
