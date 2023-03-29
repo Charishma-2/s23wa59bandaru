4e804d31f7a302fac507b8c3396ff50d773081e0
Charishma-2 <charishmachowdarybandaru@gmail.com>
2023-03-29 16:16:00 -0500

    changes made by me

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var sum=0;
-var  times=0;
+var  more=0;
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 
-  times+=1;
+  more+=1;
   sum+=times;
   res.send('Sum is: '+sum);
 });
